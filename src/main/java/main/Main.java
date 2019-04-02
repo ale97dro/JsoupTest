904f4ed0261348c5c6ff608c6f997ccd48511d48
ale97dro <alex24br@yahoo.it>
2019-04-02 19:35:48 +0200

    first test

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,32 @@
 package main;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args)
     {
-        System.out.printf("ciao");
+        System.out.print("ciao");
+
+
+        try
+        {
+            String url = "";
+            Document doc = Jsoup.connect(url).get();
+
+            //Elements elements = doc.select("div"); //Get all div elements
+
+            Elements elements = doc.getElementsByClass("entry-content"); //Get all element with the specify class
+
+            System.out.printf("ciao");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
