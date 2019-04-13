90385537bb54ee23b7538b54d2aed9f51230d813
ale97dro <alex24br@yahoo.it>
2019-04-13 15:30:23 +0200

    scraping strategy

--- src/main/java/ultimate/UltimateMain.java
+++ src/main/java/ultimate/UltimateMain.java
@@ -10,6 +10,15 @@ public class UltimateMain {
 
         Map results = scraper.scrape(null);
 
+
+        /*
+            IDEA PER TROVARE I TREND:
+                - splitto il nome del trend in base agli spazi cercando di togliere la punteggiatura (o inserisco un nome creato apposta nel DB)
+                - controllo poi che ogni valore derivante dallo split sia contenuto nel nome del megatrend (o in quello reale o in quello fittizio)
+
+
+         */
+
         System.out.println("ciao");
     }
 }
